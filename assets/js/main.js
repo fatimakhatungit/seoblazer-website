e6d60ba7c61ce38c56d6f46c59f715a0eaad29c9
fatimakhatungit <fatimakhatuncse@gmail.com>
2021-05-31 16:17:12 +0600

    added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,35 +3,35 @@
 
 
     // Reputation Management carousel
-    $(".reputation-crs").owlCarousel({
-        items: 1,
-        autoPlay: 55000,
-        stopOnHover: true,
-        center: true,
-        navigation: false,
-        pagination: true,
-        singleItem: true,
-        autoHeight: false,
-        responsive: true,
-        responsiveRefreshRate: 200,
-    });
+    // $(".reputation-crs").owlCarousel({
+    //     items: 1,
+    //     autoPlay: 55000,
+    //     stopOnHover: true,
+    //     center: true,
+    //     navigation: false,
+    //     pagination: true,
+    //     singleItem: true,
+    //     autoHeight: false,
+    //     responsive: true,
+    //     responsiveRefreshRate: 200,
+    // });
 
     // Perfect SEO Expert carousel
-    $(".perfect-seo-crs").owlCarousel({
-        items: 1,
-        autoPlay: 5500,
-        navText: [ '<span class="fa fa-angle-left">', '<span class="fa fa-angle-right">' ],
-        stopOnHover: true,
-        center: true,
-        navigation: true,
-        pagination: false,
-        rewindNav: false,
-        singleItem: true,
-        autoHeight: true,
-        responsive: true,
-        slideSpeed: 700,
-        responsiveRefreshRate: 200,
-    });
+    // $(".perfect-seo-crs").owlCarousel({
+    //     items: 1,
+    //     autoPlay: 5500,
+    //     navText: [ '<span class="fa fa-angle-left">', '<span class="fa fa-angle-right">' ],
+    //     stopOnHover: true,
+    //     center: true,
+    //     navigation: true,
+    //     pagination: false,
+    //     rewindNav: false,
+    //     singleItem: true,
+    //     autoHeight: true,
+    //     responsive: true,
+    //     slideSpeed: 700,
+    //     responsiveRefreshRate: 200,
+    // });
 
     
 
@@ -40,5 +40,7 @@
    /*END WOW ANIMATION JS*/
 
 
+
+   
 
 })(jQuery);	
